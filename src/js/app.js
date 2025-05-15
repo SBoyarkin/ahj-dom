d63eec7cbe463ed041f4ab162b6939b3db4be7d5
sboyarkin <sergey.boyarkin@mail.ru>
2025-05-15 14:02:29 +0500

    game

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,51 @@
-// TODO: write code here
+class Item {
+  constructor(tag) {
+    this.board = tag;
+  }
 
-// comment this to pass build
-const unusedVariable = "variable";
+  createItem() {
+    const newIntem = document.createElement("div");
+    newIntem.classList.add("items");
+    newIntem.dataset.id = this.board.children.length + 1;
+    this.board.append(newIntem);
+    newIntem.addEventListener("click", (event) => {
+      if (event.target.classList.contains("active-item")) {
+        this.removeActiveItem();
+      }
+    });
+  }
 
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
+  createActiveItem() {
+    const position = Math.floor(
+      this._getRandomNumber(0, this.board.children.length),
+    );
+    const items = document.querySelectorAll(".items");
+    items[position].classList.add("active-item");
+    console.log(position);
+  }
+
+  removeActiveItem(event) {
+    event = document.querySelector(".active-item");
+    if (event) {
+      event.classList.remove("active-item");
+    }
+  }
+
+  _getRandomNumber(min, max) {
+    const pos = Math.random() * (max - min) + min;
+    console.log(pos)
+    return pos;
+  }
 }
 
-console.log("app.js included");
+const mb = document.querySelector(".board");
+console.log(mb);
+const item = new Item(mb);
+
+while (mb.children.length < 16) {
+  item.createItem();
+}
+
+setInterval(() => {
+  item.removeActiveItem(), item.createActiveItem();
+}, 1000);
